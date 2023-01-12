c53a4d3ddac4cf4c93c26a54c361fc674d360e1f
Pavel Astakhov <pastakhov88@gmail.com>
2023-01-13 00:15:51 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,54 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+// 1. разбить строку на массив строк по буквам
+
+const numbersArray = [];
+for(let i = 0; i < expr.length; i = i + 10){
+    numbersArray.push(expr.substr(i, 10));
+}
+
+// 2. перекодировать каждый элелмент в код морзе
+
+const morseLetters = [];
+
+for(let morseLetter of numbersArray){
+
+    let tempString = '';
+
+    for(let i = 0; i < morseLetter.length; i++){
+        if(morseLetter[i] === '*'){
+            tempString += ' ';
+            break;
+        }
+        if(morseLetter[i] === '1'){
+            if(morseLetter[i + 1] === '0'){
+                tempString += '.';
+            }
+            if(morseLetter[i + 1] === '1'){
+                tempString += '-';
+            }
+            i++;
+        }
+    }
+
+    morseLetters.push(tempString)
+}
+
+// 3. перекодировать код морзе в буквы
+
+let result = [];
+
+for(let codedLetter of morseLetters){
+    if(MORSE_TABLE[codedLetter] === undefined){
+        result.push(' ');
+    }
+    else{
+        result.push(MORSE_TABLE[codedLetter]);
+    }
+}
+    return result.join('');
 }
 
 module.exports = {
